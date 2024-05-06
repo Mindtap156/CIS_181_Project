8a0aa2948b0564efb39f53b8a93b956d82373cb9
Mindtap156 <157560284+Mindtap156@users.noreply.github.com>
2024-05-05 23:20:36 -0500

    Add files via upload

---
+++ band_photo.js
@@ -0,0 +1,32 @@
+function whichBandMember(bandMember) {
+    switch (bandMember) {
+        case 'mira':
+            {
+                document.getElementById("band").src = "mira.jpg";
+            };
+            break;
+        case 'ryan':
+            {
+                document.getElementById("band").src = "ryan.jpg";
+            };
+            break;
+        case 'jon':
+            {
+                document.getElementById("band").src = "jon.jpg";
+            };
+            break;
+        default:
+            ;
+            break;
+    }
+
+}
+
+function notBandMember() {
+    document.getElementById("band").src = "full_band.jpg";
+}
+
+function clickInfo() {
+    let popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
